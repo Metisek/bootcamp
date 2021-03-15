17897a5f29842612b910c3a5a67c1a2377b2643f
Metisek <matboj5@gmail.com>
2021-03-15 19:56:26 +0100

    Zadania 7 aktualizacja i poprawa plików geojson

---
+++ 7 Zajęcia Python, API/test.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+from shapely.geometry import Polygon, Point
+import urllib
+import json
+
+url = 'http://api.luftdaten.info/static/v1/data.json'
+req = urllib.request.Request(url)
+resp = urllib.request.urlopen(req)
+content = resp.read()
+data = json.loads(content)
+
+g = open('przemyslGranice.geojson', 'r')
+granice = g.read()
+granice = json.loads(granice)
+
+lista = granice['features'][0]['geometry']['coordinates'][0]
+polyg = []
+for i in range(0, len(lista)):
+    polyg.append(tuple(lista[i]))
+
+polyg = Polygon(polyg)
+
+file = open('Czujniki_Przemysl.geojson', 'w')
+geolista = []
+
+for i in data:
+    lat = i['location']['latitude']
+    lon = i['location']['longitude']
+    point = Point(float(lon), float(lat))
+    if point.within(polyg) == True:
+        geolista.append('a')
+        geolista.append('{"type": "Feature", "geometry":{"type:"Point", "coordinates":[{}, {}]}, "properties":{"id":{}} }'.format(lat, lon, 123))    
+
+wyjscie = '{"type": "FeatureCollection","features":{}}'.format(geolista)
